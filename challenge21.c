d1ccf15b3832fd5e5fb3d0880a9999294152ff40
Anass-Dr <anasdrissi77@gmail.com>
2023-09-16 11:22:17 +0100

    Challenge 21 modified

--- challenge21.c
+++ challenge21.c
@@ -7,13 +7,17 @@ int main()
     printf("Entrer le nombre de lignes: ");
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         for (int j = n - i; j > 0; j--)
         {
             printf(" ");
         }
-        for (int x = 1; x <= i; x++)
+        for (int x = 1; x <= i + 1; x++)
+        {
+            printf("*");
+        }
+        for (int z = 0; z < i; z++)
         {
             printf("*");
         }
